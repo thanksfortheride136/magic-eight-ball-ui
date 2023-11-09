4969500a2214b6f24750e839f8eadf3fe62d4fa7
Louis Cooper <104042225+thanksfortheride136@users.noreply.github.com>
2023-11-09 15:14:31 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('askButton').addEventListener('click', function(){
+    let answer = '';
+    let randomIndex = Math.floor(Math.random()* 5);
+
+    if(randomIndex === 0){
+        answer = "It is certain";
+    } else if (randomIndex === 1){
+        answer = "Ask again later";
+    } else if (randomIndex === 2){
+        answer = "Yes, definitely";
+      else if (randomIndex === 3){
+        answer = "My reply is, no";
+      }
+    }
+});
